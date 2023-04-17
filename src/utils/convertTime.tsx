d86dd9769d8040a58a535f788dd797bf4df9ae35
Lexx-Dev <alexcalle.paola@gmail.com>
2023-04-16 21:17:07 -0500

    utils

---
+++ src/utils/convertTime.tsx
@@ -0,0 +1,5 @@
+export const minToSeconds = (min: number) => min * 60
+export const hoursToSeconds = (hours: number) => hours * 60 * 60
+export const daysToSeconds = (days: number) => days * 24 * 60 * 60
+export const weeksToSeconds = (weeks: number) => weeks * 7 * 24 * 60 * 60
+export const monthsToSeconds = (months: number) => months * 30 * 24 * 60 * 60
