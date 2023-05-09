991939ba2e4b3fb64652a555dc1916d5b1a444de
Lexx-Dev <alexcalle.paola@gmail.com>
2023-05-09 14:08:39 -0500

    move some enums to enum folder

--- src/constants.ts
+++ src/constants.ts
@@ -1,20 +1,26 @@
 // const API_BASE_URL = 'https://anime-scrapper-two.vercel.app/api'
-const API_BASE_URL = 'http://localhost:3002/api'
-export const IMG_DOMAIN = 'anime-app.s3.us-east-1.amazonaws.com'
-
-export const API = {
-  baseUrl: API_BASE_URL,
-  routes: {
-    LatestEpisodes: `${API_BASE_URL}/episodes/latest`,
-    VideoStreaming: `${API_BASE_URL}/episodes/sources`,
-    LatestAnimes: `${API_BASE_URL}/animes/latest`,
-    BroadcastAnimes: `${API_BASE_URL}/animes/broadcast`,
-    RatingAnimes: `${API_BASE_URL}/animes/latest/rating`,
-    InfoAnime: `${API_BASE_URL}/animes`
-  },
-}
+export const API_BASE_URL = 'http://localhost:3002/api'
+export const IMG_DOMAIN = /anime-app.s3|cdn.animeflv.net/
 
 export const APP_ROUTES = [
-  { name: 'Home', link: '/' },
-  { name: 'Directory', link: '/directory' },
+  {
+    name: 'Home',
+    link: '/',
+    description: 'View most recent episodes and animes',
+  },
+  {
+    name: 'Directory',
+    link: '/directory',
+    description: 'View all animes',
+  },
+  {
+    name: 'Animes',
+    link: '/animes',
+    description: 'View Animes',
+  },
+  {
+    name: 'Episodes',
+    link: '/episodes',
+    description: 'View episodes',
+  },
 ]
--- src/enums.ts
+++ src/enums.ts
@@ -1,11 +1,34 @@
+import { API_BASE_URL } from './constants'
+
 export enum ResponseType {
   JSON = 'json',
   TEXT = 'text',
-  BUFFER = 'buffer'
+  BUFFER = 'buffer',
 }
 
 export enum animeStatus {
   BROADCAST = '1',
   FINALIZED = '2',
-  SOON = '3'
+  SOON = '3',
+}
+
+export enum APIRoutes {
+  baseUrl = API_BASE_URL,
+  LatestEpisodes = `${API_BASE_URL}/episodes/latest`,
+  VideoStreaming = `${API_BASE_URL}/episodes/sources`,
+  LatestAnimes = `${API_BASE_URL}/animes/latest`,
+  BroadcastAnimes = `${API_BASE_URL}/animes/broadcast`,
+  RatingAnimes = `${API_BASE_URL}/animes/latest/rating`,
+  InfoAnime = `${API_BASE_URL}/animes`,
+  AnimeEpisodes = `${API_BASE_URL}/animes/:animeId/episodes`,
+  SearchAnimes = `${API_BASE_URL}/animes/search`,
+}
+
+export enum autoCompleteHotKeys {
+  ENTER = 'enter',
+  ESCAPE = 'escape',
+  ARROW_DOWN = 'arrowdown',
+  ARROW_UP = 'arrowup',
+  CTRL_SPACE = 'ctrl+space',
+  CTRL_K = 'ctrl+k',
 }
