fd56aa8224238b05a65c098746a9ea313c7ac580
Lexx-Dev <alexcalle.paola@gmail.com>
2023-05-09 14:09:33 -0500

    add favicons and font options

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,29 @@
-import Header from '@/components/Header'
+import Header from '@/components/Header/Header'
 import { APP_ROUTES } from '@/constants'
+import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
-import '../styles/globals.css'
 
 export const metadata = {
   title: 'Anime App',
-  description: 'This a web app to see favorite animes',
+  description: 'This a web app to see your favorite animes',
+  icons: {
+    icon: '/Nika_Logo.svg',
+    apple: '/Nika_Logo.svg',
+  },
   keywords: ['anime', 'app', 'nextjs'],
 }
 
 const interFont = Inter({
   subsets: ['latin'],
   variable: '--inter-font',
+  preload: true,
+  adjustFontFallback: true,
 })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={interFont.variable}>
+      <body className={interFont.className}>
         <Header pages={APP_ROUTES} />
         {children}
       </body>
