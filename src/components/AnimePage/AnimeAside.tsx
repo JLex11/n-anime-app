6327eeadd95cdda44e17ec7b85831de1d8ff7b2e
Lexx-Dev <alexcalle.paola@gmail.com>
2023-05-09 14:11:04 -0500

    add aside to home page

---
+++ src/components/AnimePage/AnimeAside.tsx
@@ -0,0 +1,35 @@
+import styles from '@/styles/Anime.module.css'
+import { placeholderImgs } from '@/utils/placeHolderImgs'
+import Image from 'next/image'
+
+interface Props {
+  image: string
+  status: string | undefined
+  title: string
+}
+
+export const AnimeAside = ({ image, status, title }: Props) => {
+  const aspectRatio = 9 / 12
+  const placeholderImg = placeholderImgs.at(0)
+
+  return (
+    <section className={styles.aside}>
+      <Image
+        src={image}
+        alt={title}
+        width={300}
+        height={300 / aspectRatio}
+        className={styles.asideImg}
+        priority
+        loading='eager'
+        blurDataURL={placeholderImg}
+        placeholder='blur'
+      />
+      {status && (
+        <div className={styles.status}>
+          <span className={styles.statusValue}>{status}</span>
+        </div>
+      )}
+    </section>
+  )
+}
