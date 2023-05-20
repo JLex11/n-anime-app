1d71ad5c837097badfdf7046139d4172470744b8
Lexx-Dev <alexcalle.paola@gmail.com>
2023-05-19 21:34:35 -0500

    add episode component

---
+++ src/components/AnimePage/Episodes.tsx
@@ -0,0 +1,28 @@
+import styles from '@/styles/Anime.module.css'
+import { Episode } from '@/types'
+import clsx from 'clsx'
+import Image from 'next/image'
+import Link from 'next/link'
+
+interface EpisodesProps {
+  episodes: Episode[]
+  animeTitle: string
+}
+
+export const Episodes = ({ episodes, animeTitle }: EpisodesProps) => {
+  const episodesClass = clsx(styles.sectionGrid, styles.episodesList)
+
+  return (
+    <section className={styles.section}>
+      <h2 className={styles.sectionTitle}>Episodes</h2>
+      <div className={episodesClass}>
+        {episodes.map(episode => (
+          <Link key={episode.episodeId} href={`/episodes/${episode.episodeId}`}>
+            <Image src={episode.image ?? ''} alt={`Episode ${episode.episode} of ${animeTitle}`} width={150} height={100} loading='lazy' />
+            <span className={styles.episodeNumber}>Episode {episode.episode}</span>
+          </Link>
+        ))}
+      </div>
+    </section>
+  )
+}
