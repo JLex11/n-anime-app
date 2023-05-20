f5ae97ca60662f1d92bc52f861ac02992153eec9
Lexx-Dev <alexcalle.paola@gmail.com>
2023-05-19 21:36:36 -0500

    add grid container for cards

---
+++ src/components/Common/GridContainer.tsx
@@ -0,0 +1,28 @@
+import styles from '@/styles/GridContainer.module.css'
+
+interface Props {
+  children: React.ReactNode
+  width?: number
+  height?: number
+  gap?: string
+}
+
+interface CSSProperties extends React.CSSProperties {
+  '--grid-width'?: string
+  '--grid-height'?: string
+  '--grid-gap'?: string
+}
+
+export const GridContainer = ({ children, width, height, gap }: Props) => {
+  const gridStyles: CSSProperties = {
+    '--grid-width': `${width || 270}px`,
+    '--grid-height': `${height || 450}px`,
+    '--grid-gap': gap || '2rem',
+  }
+
+  return (
+    <div className={styles.gridContainer} style={gridStyles}>
+      {children}
+    </div>
+  )
+}
