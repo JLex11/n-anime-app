b8d4074dc4779e764c4337090316ff119e087285
Lexx-Dev <alexcalle.paola@gmail.com>
2023-05-19 21:36:20 -0500

    add breadcrumb

---
+++ src/components/Common/BreadCrumb.tsx
@@ -0,0 +1,26 @@
+import styles from '@/styles/CustomElements.module.css'
+import Link from 'next/link'
+
+type Crumb = {
+  name: string
+  path?: string
+}
+
+interface Props {
+  crumbs: Crumb[]
+}
+
+export const BreadCrumb = ({ crumbs }: Props) => {
+  return (
+    <div className={styles.breadcrumbContainer}>
+      <ul className={styles.breadcrumbList}>
+        {crumbs.map(({ name, path }, index) => (
+          <li key={path ?? name} className={styles.breadcrumbItem}>
+            {path ? <Link href={path}>{name}</Link> : <span>{name}</span>}
+            {/* {index < crumbs.length - 1 && <span className={styles.separator}>/</span>} */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
