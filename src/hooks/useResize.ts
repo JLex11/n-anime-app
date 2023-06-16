df4765ad899bed591998f42be067620d05257522
Lexx-Dev <alexcalle.paola@gmail.com>
2023-06-16 08:36:10 -0500

    create hook useResize to use in a future

---
+++ src/hooks/useResize.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+
+export const useResize = (ref: React.RefObject<HTMLElement>) => {
+  const [size, setSize] = useState({
+    width: 0,
+    height: 0,
+  })
+
+  useEffect(() => {
+    if (ref.current) {
+      setSize({
+        width: ref.current.offsetWidth,
+        height: ref.current.offsetHeight,
+      })
+    }
+  }, [ref])
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (ref.current) {
+        setSize({
+          width: ref.current.offsetWidth,
+          height: ref.current.offsetHeight,
+        })
+      }
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [ref])
+
+  return size
+}
