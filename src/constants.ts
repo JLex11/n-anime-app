8b64263cb143fbb14c03b8f80b1a01a40f35fd34
Lexx-Dev <alexcalle.paola@gmail.com>
2023-06-25 20:01:55 -0500

    change api url

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://anime-scrapper-two.vercel.app/api'
+export const API_BASE_URL = 'https://anime-scrapper-jlex11.vercel.app/api'
 //export const API_BASE_URL = 'http://localhost:3002/api'
 export const IMG_DOMAIN = /anime-app.s3|cdn.animeflv.net/
 
