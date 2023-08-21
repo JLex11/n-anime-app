5faa520fc703cae7692cc653a65b1537ec64cbef
Lexx-Dev <alexcalle.paola@gmail.com>
2023-08-21 13:35:53 -0500

    change cache time

--- src/services/getLatestAnimes.tsx
+++ src/services/getLatestAnimes.tsx
@@ -1,11 +1,11 @@
 import { APIRoutes } from '@/enums'
 import { Anime } from '@/types'
-import { minToSeconds } from '@/utils/convertTime'
+import { hoursToSeconds } from '@/utils/convertTime'
 import { filterUnsupportDomains } from '../utils/filterUnsupportDomains'
 
 export const getLatestAnimes = async (limit = 10): Promise<Anime[]> => {
   const response = await fetch(`${APIRoutes.LatestAnimes}?limit=${limit}`, {
-    next: { revalidate: minToSeconds(10) },
+    next: { revalidate: hoursToSeconds(6) },
   })
 
   const animes: Anime[] = await response.json()
