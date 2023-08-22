d0b434bf4db335f48b5ffaac04f258a809a29155
Lexx-Dev <alexcalle.paola@gmail.com>
2023-08-22 18:44:53 -0500

    add imageConfig

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,37 @@
 export const API_BASE_URL = 'https://anime-scrapper-alpha.vercel.app/api'
 //export const API_BASE_URL = 'http://localhost:3002/api'
-export const IMG_DOMAIN = /anime-app.s3|cdn.animeflv.net|anime-scrapper-alpha.vercel.app|www3.animeflv.net/
+
+const IMAGE_DAYS_CACHE = 30
+
+export const imageConfig = {
+  minimumCacheTTL: IMAGE_DAYS_CACHE * 24 * 60 * 60,
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'anime-app.s3.us-east-1.amazonaws.com',
+      port: '',
+      pathname: '/**',
+    },
+    {
+      protocol: 'http',
+      hostname: 'anime-scrapper-alpha.vercel.app',
+      port: '',
+      pathname: '/api/image/**',
+    },
+    {
+      protocol: 'https',
+      hostname: 'cdn.animeflv.net',
+      port: '',
+      pathname: '/screenshots/**',
+    },
+    {
+      protocol: 'https',
+      hostname: 'www3.animeflv.net',
+      port: '',
+      pathname: '/uploads/animes/covers/**',
+    },
+  ],
+}
 
 export const APP_ROUTES = [
   {
