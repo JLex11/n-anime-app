7d9bc7d315f6472f423675034a131fb9433a0353
Lexx-Dev <alexcalle.paola@gmail.com>
2023-08-25 17:14:08 -0500

    fix ouput type

--- src/hooks/useFallbackImage.ts
+++ src/hooks/useFallbackImage.ts
@@ -7,21 +7,33 @@ interface Image {
   position?: string
 }
 
+interface OutputImage extends Image {
+  link: string
+}
+
+type DefaultDimensions = {
+  width: number
+  height: number
+}
+
 type HandleImageError = (e: React.SyntheticEvent<HTMLImageElement>) => void
 
-export const useFallbackImage = (images: Image[]) => {
+export const useFallbackImage = (images: Image[], defaultDimensions: DefaultDimensions) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0)
-  const indexedImages = [...images, { link: '/lights-blur.webp' }]
+  const indexedImages = [
+    ...(images.filter(({ link }) => Boolean(link)) as OutputImage[]),
+    { link: '/lights-blur.webp' }
+  ]
 
   const handleImageError: HandleImageError = () => {
     setCurrentImageIndex(prevCurrentImage => prevCurrentImage + 1)
   }
 
   const currentImage = {
-    link: indexedImages[currentImageIndex]?.link,
-    width: (indexedImages[currentImageIndex]?.width || 0) * 0.75,
-    height: (indexedImages[currentImageIndex]?.height || 0) * 0.75,
-    position: indexedImages[currentImageIndex]?.position ?? 'center',
+    link: indexedImages[currentImageIndex].link,
+    width: (indexedImages[currentImageIndex]?.width || defaultDimensions.width) * 0.75,
+    height: (indexedImages[currentImageIndex]?.height || defaultDimensions.height) * 0.75,
+    position: indexedImages[currentImageIndex]?.position ?? 'center'
   }
 
   return { currentImage: currentImage, onError: handleImageError }
