441fc3a5b78b217351ab4fe7f46ac9b7933bc0a7
Lexx-Dev <alexcalle.paola@gmail.com>
2023-08-26 21:11:30 -0500

    remove inneed domains

--- next.config.js
+++ next.config.js
@@ -7,31 +7,13 @@ const nextConfig = {
     minimumCacheTTL: IMAGE_DAYS_CACHE * 24 * 60 * 60,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'anime-app.s3.us-east-1.amazonaws.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
         protocol: 'http',
         hostname: 'anime-scrapper-alpha.vercel.app',
         port: '',
-        pathname: '/api/image/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.animeflv.net',
-        port: '',
-        pathname: '/screenshots/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www3.animeflv.net',
-        port: '',
-        pathname: '/uploads/animes/covers/**',
-      },
-    ],
-  },
+        pathname: '/api/image/**'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
--- src/constants.ts
+++ src/constants.ts
@@ -1,37 +1,14 @@
 export const API_BASE_URL = 'https://anime-scrapper-alpha.vercel.app/api'
 //export const API_BASE_URL = 'http://localhost:3002/api'
 
-const IMAGE_DAYS_CACHE = 30
-
-export const imageConfig = {
-  minimumCacheTTL: IMAGE_DAYS_CACHE * 24 * 60 * 60,
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'anime-app.s3.us-east-1.amazonaws.com',
-      port: '',
-      pathname: '/**',
-    },
-    {
-      protocol: 'http',
-      hostname: 'anime-scrapper-alpha.vercel.app',
-      port: '',
-      pathname: '/api/image/**',
-    },
-    {
-      protocol: 'https',
-      hostname: 'cdn.animeflv.net',
-      port: '',
-      pathname: '/screenshots/**',
-    },
-    {
-      protocol: 'https',
-      hostname: 'www3.animeflv.net',
-      port: '',
-      pathname: '/uploads/animes/covers/**',
-    },
-  ],
-}
+export const imageDomains = [
+  {
+    protocol: 'http',
+    hostname: 'anime-scrapper-alpha.vercel.app',
+    port: '',
+    pathname: '/api/image/**'
+  }
+]
 
 export const APP_ROUTES = [
   {
