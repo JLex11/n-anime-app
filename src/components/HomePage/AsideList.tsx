5770ed8c077b123cf587f56661a839055b840bfc
Lexx-Dev <alexcalle.paola@gmail.com>
2023-08-28 22:02:27 -0500

    move fetching to improve html streaming

--- src/components/HomePage/AsideList.tsx
+++ src/components/HomePage/AsideList.tsx
@@ -1,11 +1,13 @@
-import { Anime } from '@/types'
+import { getBroadcastAnimes } from '@/services/getBroadcastAnimes'
 import Link from 'next/link'
 import styles from './Home.module.css'
 
-export const AsideList = ({ animes }: { animes: Anime[] }) => {
+export const AsideList = async () => {
+  const broadcastAnimes = await getBroadcastAnimes(30)
+
   return (
     <ul className={styles.asideList}>
-      {animes.map(anime => (
+      {broadcastAnimes.map(anime => (
         <li key={anime.animeId}>
           <Link href={`/animes/${anime.animeId}`}>{anime.title}</Link>
         </li>
--- src/components/HomePage/HomeAside.tsx
+++ src/components/HomePage/HomeAside.tsx
@@ -1,18 +1,18 @@
-import { getBroadcastAnimes } from '@/services/getBroadcastAnimes'
+import { Suspense } from 'react'
 import BroadcastIcon from '../Icons/BroadcastIcon'
 import { AsideList } from './AsideList'
 import styles from './Home.module.css'
 
-export const HomeAside = async () => {
-  const broadcastAnimes = await getBroadcastAnimes(30)
-
+export const HomeAside = () => {
   return (
     <aside className={styles.aside} id={'home-aside'}>
       <h2 className={styles.title}>
         <BroadcastIcon /> En emisión
       </h2>
       <div className={styles.asideInner}>
-        <AsideList animes={broadcastAnimes} />
+        <Suspense fallback={<span>Loading...</span>}>
+          <AsideList />
+        </Suspense>
       </div>
     </aside>
   )
