3bc57596c8fddfe401c3aefa213afaed3608d3fb
Lexx-Dev <alexcalle.paola@gmail.com>
2023-08-31 22:30:23 -0500

    create useResizeObserver hook

---
+++ src/hooks/useResizeObserver.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+
+export const useResizeObserver = (target?: Element | null) => {
+  const [elementSizes, setElementSizes] = useState<ResizeObserverSize>({ inlineSize: 0, blockSize: 0 })
+
+  useEffect(() => {
+    if (!target) return
+
+    const handleResize: ResizeObserverCallback = e => {
+      const [firstEntry] = e
+      const [borderSizes] = firstEntry.borderBoxSize
+      setElementSizes(borderSizes)
+    }
+
+    const observer = new ResizeObserver(handleResize)
+    observer.observe(target)
+
+    return () => observer.disconnect()
+  }, [target])
+
+  return elementSizes
+}
