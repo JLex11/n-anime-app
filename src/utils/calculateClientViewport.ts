2f18d0587d5324fdf36df3f012635a9b43f05574
Lexx-Dev <alexcalle.paola@gmail.com>
2023-09-10 01:27:16 -0500

    reduce white lines

--- src/utils/calculateClientViewport.ts
+++ src/utils/calculateClientViewport.ts
@@ -9,8 +9,6 @@ export const viewHeight = (percent: number) => viewSize(percent, ViewportSize.He
 
 const viewSize = (percent: number, size: ViewportSize) => {
   if (typeof window === 'undefined') return 0
-
   const documentSize = Math.max(document.documentElement[`client${size}`], window[`inner${size}`] || 0)
-
   return (percent * documentSize) / 100
 }
