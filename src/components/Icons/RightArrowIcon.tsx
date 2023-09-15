a38fc5c4ecbc0bbf918f58498910128a2ca74e8b
Lexx-Dev <alexcalle.paola@gmail.com>
2023-09-15 18:42:26 -0500

    add a new icon

---
+++ src/components/Icons/RightArrowIcon.tsx
@@ -0,0 +1,27 @@
+interface Props {
+  width?: number
+  height?: number
+  strokeWidth?: number
+  className?: string
+}
+
+export default function RightArrowIcon({ width = 24, height, strokeWidth = 1.5, className: cssClass }: Props) {
+  return (
+    <svg
+      className={cssClass}
+      width={width}
+      height={height || width}
+      viewBox='0 0 24 24'
+      strokeWidth={strokeWidth}
+      stroke='currentColor'
+      fill='none'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+      <path d='M5 12l14 0'></path>
+      <path d='M15 16l4 -4'></path>
+      <path d='M15 8l4 4'></path>
+    </svg>
+  )
+}
