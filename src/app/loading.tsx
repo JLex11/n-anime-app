a898baba78f4886a9a6f245c993648f9261d9ef0
Lexx-Dev <alexcalle.paola@gmail.com>
2023-09-15 18:45:02 -0500

    remove ugly loading

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import LoadingIcon from '@/components/Icons/LoadingIcon'
-
-export default function Loading() {
-  return (
-    <div className='page-loader' style={{ height: '100vh' }}>
-      <LoadingIcon />
-    </div>
-  )
-}
