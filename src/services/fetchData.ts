ec0c45ca8f5e3da313aa0ac60913341b0cd1aad1
Alex Calle <chikyelfurioso2011@gmail.com>
2023-12-09 10:50:20 -0500

    remove alternative fetching

--- src/services/fetchData.ts
+++ src/services/fetchData.ts
@@ -1,33 +1,11 @@
 import { APIRoutes } from '@/enums'
 
 interface NextFetchInit extends RequestInit {
-  next: any
+  next?: any
 }
 
 const responseJson = (response: Response) => response.json()
 
-const handleError = (error: unknown) => {
-  if (error instanceof DOMException && error.name === 'AbortError') {
-    // console.error('Fetch request was aborted')
-  } else {
-    throw error
-  }
-}
-
-export const fetchData = async (apiPath: string, fetchConfig: RequestInit = {}) => {
-  const controller = new AbortController()
-
-  const promiseArray = [
-    fetch(`${APIRoutes.vercelBaseUrl}${apiPath}`, { ...fetchConfig, signal: controller.signal })
-      .then(responseJson)
-      .catch(handleError),
-    fetch(`${APIRoutes.fl0BaseUrl}${apiPath}`, { ...fetchConfig, signal: controller.signal })
-      .then(responseJson)
-      .catch(handleError)
-  ]
-
-  const response = await Promise.race(promiseArray)
-  controller.abort()
-
-  return response
+export const fetchData = async (apiPath: string, fetchConfig: NextFetchInit = {}) => {
+  return fetch(`${APIRoutes.vercelBaseUrl}${apiPath}`, { ...fetchConfig }).then(responseJson)
 }
