d33a90e4f7104d032f05623230af31938d5781af
Alex Calle <chikyelfurioso2011@gmail.com>
2023-12-09 19:56:38 -0500

    Add biome.json and update dependencies and next.config.js

--- next.config.js
+++ next.config.js
@@ -2,36 +2,41 @@
 
 const IMAGE_DAYS_CACHE = 30
 
+const images = {
+	minimumCacheTTL: IMAGE_DAYS_CACHE * 24 * 60 * 60,
+	remotePatterns: [
+		{
+			protocol: "https",
+			hostname: "anime-scrapper-alpha.vercel.app",
+			port: "",
+			pathname: "/api/image/**",
+		},
+		{
+			protocol: "http",
+			hostname: "anime-scrapper-alpha.vercel.app",
+			port: "",
+			pathname: "/api/image/**",
+		},
+		{
+			protocol: "http",
+			hostname: "anime-scrapper-2rl4-dev.fl0.io",
+			port: "",
+			pathname: "/api/image/**",
+		},
+		{
+			protocol: "https",
+			hostname: "anime-scrapper-2rl4-dev.fl0.io",
+			port: "",
+			pathname: "/api/image/**",
+		},
+	],
+}
+
 const nextConfig = {
-  images: {
-    minimumCacheTTL: IMAGE_DAYS_CACHE * 24 * 60 * 60,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'anime-scrapper-alpha.vercel.app',
-        port: '',
-        pathname: '/api/image/**'
-      },
-      {
-        protocol: 'http',
-        hostname: 'anime-scrapper-alpha.vercel.app',
-        port: '',
-        pathname: '/api/image/**'
-      },
-      {
-        protocol: 'http',
-        hostname: 'anime-scrapper-2rl4-dev.fl0.io',
-        port: '',
-        pathname: '/api/image/**'
-      },
-      {
-        protocol: 'https',
-        hostname: 'anime-scrapper-2rl4-dev.fl0.io',
-        port: '',
-        pathname: '/api/image/**'
-      }
-    ]
-  }
+	images,
+	experimental: {
+		ppr: true,
+	},
 }
 
 module.exports = nextConfig
