4f0310b0de0ede2c4e71e8afc5a2aa0c1ec771f0
Alex Calle <chikyelfurioso2011@gmail.com>
2023-12-15 16:12:10 -0500

    improve types and use useFallbackImage hook

--- src/hooks/useFallbackImage.ts
+++ src/hooks/useFallbackImage.ts
@@ -1,40 +1,35 @@
-import { useState } from 'react'
+import { useState } from "react"
 
 interface Image {
-  link?: string | null
-  width?: number
-  height?: number
-  position?: string
+	link?: string | null
+	width?: number
+	height?: number
+	position?: string
 }
 
 interface OutputImage extends Image {
-  link: string
+	link: string
 }
 
 type DefaultDimensions = {
-  width: number
-  height: number
+	width: number
+	height: number
 }
 
 type HandleImageError = (e: React.SyntheticEvent<HTMLImageElement>) => void
 
 export const useFallbackImage = (images: Image[], defaultDimensions: DefaultDimensions) => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0)
-  const indexedImages = [
-    ...(images.filter(({ link }) => Boolean(link)) as OutputImage[]),
-    { link: '/lights-blur.webp' }
-  ]
+	const [currentImageIndex, setCurrentImageIndex] = useState(0)
+	const indexedImages = images.filter((image): image is OutputImage => Boolean(image.link))
 
-  const handleImageError: HandleImageError = () => {
-    setCurrentImageIndex(prevCurrentImage => prevCurrentImage + 1)
-  }
+	const handleImageError: HandleImageError = () => setCurrentImageIndex(prevCurrentImage => prevCurrentImage + 1)
 
-  const currentImage = {
-    link: indexedImages[currentImageIndex].link,
-    width: indexedImages[currentImageIndex]?.width || defaultDimensions.width /*  * 0.75 */,
-    height: indexedImages[currentImageIndex]?.height || defaultDimensions.height /*  * 0.75 */,
-    position: indexedImages[currentImageIndex]?.position ?? 'center'
-  }
+	const currentImage = {
+		link: indexedImages[currentImageIndex].link,
+		width: indexedImages[currentImageIndex].width || defaultDimensions.width,
+		height: indexedImages[currentImageIndex].height || defaultDimensions.height,
+		position: indexedImages[currentImageIndex].position ?? "center",
+	}
 
-  return { currentImage: currentImage, onError: handleImageError }
+	return { currentImage: currentImage, onError: handleImageError }
 }
