e2ac4cc44ee6c116d3609c69a750663e6651b5f9
Alex Calle <chikyelfurioso2011@gmail.com>
2023-12-16 12:18:55 -0500

    Refactor AutocompleteInput component

--- src/components/ServerAutocomplete/Input.tsx
+++ src/components/ServerAutocomplete/Input.tsx
@@ -9,32 +9,33 @@ import styles from './ServerAutocomplete.module.css'
 import { AutocompleteVars } from './autocompleteTypes'
 
 interface InputProps {
-  query: string
+	query: string
 }
 
 export function AutocompleteInput({ query }: InputProps) {
-  const [queryValue, setQueryValue] = useState('')
-  const searchQuery = useDebounce(queryValue, 300)
-  const router = useRouter()
+	const [queryValue, setQueryValue] = useState('')
+	const searchQuery = useDebounce(queryValue, 300)
+	const router = useRouter()
 
-  useEffect(() => {
-    router.push(`?${AutocompleteVars.QUERY_NAME}=${searchQuery}`)
-  }, [searchQuery, router, query])
+	useEffect(() => {
+		router.push(`?${AutocompleteVars.QUERY_NAME}=${searchQuery}`)
+	}, [searchQuery, router])
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { target } = e
-    setQueryValue(target.value)
-  }
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { target } = e
+		setQueryValue(target.value)
+	}
 
-  const inputContainerClass = clsx(styles.inputContainer, query.length > 0 && styles.active)
+	const inputContainerClass = clsx(styles.inputContainer, query.length > 0 && styles.active)
 
-  return (
-    <div className={inputContainerClass}>
-      <input className={styles.inputSearch} type='search' defaultValue={query} onChange={handleChange} autoFocus />
-      <div className={styles.containerInputIcons}>
-        {/* {status === 'loading' && <LoadingIcon className={loadingIconClass} />} */}
-        {/* status !== 'loading' &&  */ <ShortcutLetter letters='esc' className={styles.inputIcon} />}
-      </div>
-    </div>
-  )
+	return (
+		<div className={inputContainerClass}>
+			{/* biome-ignore lint/a11y/noAutofocus: <explanation> */}
+			<input className={styles.inputSearch} type="search" defaultValue={query} onChange={handleChange} autoFocus />
+			<div className={styles.containerInputIcons}>
+				{/* {status === 'loading' && <LoadingIcon className={loadingIconClass} />} */}
+				{/* status !== 'loading' &&  */ <ShortcutLetter letters="esc" className={styles.inputIcon} />}
+			</div>
+		</div>
+	)
 }
