ebcdbd453298a708cf98ef45756c23e2253f034d
Alex Calle <chikyelfurioso2011@gmail.com>
2024-01-11 19:35:20 -0500

    Refactor fetchData function to handle errors and abort fetch requests

--- src/services/fetchData.ts
+++ src/services/fetchData.ts
@@ -1,33 +1,22 @@
 import { APIRoutes } from '@/enums'
 
 interface NextFetchInit extends RequestInit {
-  next?: any
+  next?: Record<string, unknown>
 }
 
 const responseJson = (response: Response) => response.json()
-
-const handleError = (error: unknown) => {
-  if (error instanceof DOMException && error.name === 'AbortError') {
-    // console.error('Fetch request was aborted')
-  } else {
-    throw error
-  }
-}
 
 export const fetchData = async (apiPath: string, fetchConfig: NextFetchInit = {}) => {
   const controller = new AbortController()
 
   const promiseArray = [
+    fetch(`${APIRoutes.renderBaseUrl}${apiPath}`, { ...fetchConfig, signal: controller.signal }),
     fetch(`${APIRoutes.vercelBaseUrl}${apiPath}`, { ...fetchConfig, signal: controller.signal })
-      .then(responseJson)
-      .catch(handleError),
-    fetch(`${APIRoutes.renderBaseUrl}${apiPath}`, { ...fetchConfig, signal: controller.signal })
-      .then(responseJson)
-      .catch(handleError)
   ]
 
   const response = await Promise.race(promiseArray)
+    .then(responseJson, (error) => console.error(error))
+    .finally(() => controller.abort())
 
-  controller.abort()
   return response
 }
