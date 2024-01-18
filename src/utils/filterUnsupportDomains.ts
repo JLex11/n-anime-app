4e0efe43d7c3bf0e5f488ad964439270e58ebb63
Alex Calle <chikyelfurioso2011@gmail.com>
2024-01-17 21:16:54 -0500

    Refactor image URL handling in filterUnsupportDomains.ts

--- src/utils/filterUnsupportDomains.ts
+++ src/utils/filterUnsupportDomains.ts
@@ -1,20 +1,31 @@
-import { imageDomains } from "@/constants"
-import { Anime } from "@/types"
+import { imageDomains } from '@/constants'
+import { Anime } from '@/types'
 
 const domains = imageDomains.map(({ hostname }) => hostname)
-const domainsRegexp = new RegExp(domains.join("|"), "i")
+const domainsRegexp = new RegExp(domains.join('|'), 'i')
 
-const isValidDomain = (url?: string | null) => domainsRegexp.test(url ?? "")
+const isValidDomain = (url?: string | null) => domainsRegexp.test(url ?? '')
+
+const replaceInvalidImagePath = (url: string) =>
+	url && !url.includes('/api/image/') ? url.replace('/api/', '/api/image/') : url
 
 export const filterUnsupportDomains = (anime: Anime) => {
 	const { images } = anime
+	const coverImage = images?.coverImage ?? ''
+	const carouselImages = images?.carouselImages ?? []
 
 	return {
 		...anime,
 		images: {
-			coverImage: isValidDomain(images?.coverImage) ? images?.coverImage : null,
-			carouselImages:
-				images?.carouselImages?.filter(img => isValidDomain(img.link)) || [],
-		},
+			coverImage: isValidDomain(coverImage) ? replaceInvalidImagePath(coverImage) : '',
+			carouselImages: carouselImages
+				.filter(img => isValidDomain(img.link))
+				.map(img => {
+					return {
+						...img,
+						link: replaceInvalidImagePath(img.link)
+					}
+				})
+		}
 	}
 }
