0680b18eea7e93041496d2dd66244e499722cfa5
Alex Calle <chikyelfurioso2011@gmail.com>
2024-01-18 15:37:22 -0500

    supports custom styles

--- src/components/CardsSection/index.tsx
+++ src/components/CardsSection/index.tsx
@@ -7,15 +7,17 @@ interface Props {
   icon?: React.ReactNode
   gridWidth?: number
   gridHeight?: number
+  gridGap?: string
+  style?: React.CSSProperties
   children: React.ReactNode
 }
 
-export function CardsSection({ title, icon, gridWidth, gridHeight, children }: Props) {
+export function CardsSection({ title, icon, gridWidth, gridHeight, gridGap, style, children }: Props) {
   return (
-    <section className={styles.latestSection}>
+    <section className={styles.latestSection} style={style}>
       <div className={styles.content}>
         {!!title && <CardsSectionHeader title={title} icon={icon} />}
-        <GridContainer width={gridWidth} height={gridHeight}>
+        <GridContainer width={gridWidth} height={gridHeight} gap={gridGap} >
           {children}
         </GridContainer>
       </div>
