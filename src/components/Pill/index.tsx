185fd279944826057472612d263fa95eb999c4b4
Alex Calle <chikyelfurioso2011@gmail.com>
2024-01-17 21:17:32 -0500

    Add Pill component with styling

---
+++ src/components/Pill/index.tsx
@@ -0,0 +1,30 @@
+import clsx from 'clsx'
+import styles from './Pill.module.css'
+
+interface CSSProperties extends React.CSSProperties {
+  '--pill-color'?: string
+  '--pill-bg'?: string
+}
+
+export interface PillProps {
+  label?: string
+  color?: string
+  bgColor?: string
+  className?: string
+}
+
+const dfGradient = 'linear-gradient(175deg, #ffb800, #c38c00)'
+const dfColor = '#fff'
+
+export function Pill({ label, color = dfColor, bgColor = dfGradient, className }: PillProps) {
+  const pillStyles: CSSProperties = {
+    '--pill-color': color,
+    '--pill-bg': bgColor
+  }
+
+  return (
+    <span className={clsx(styles.pill, className)} style={pillStyles}>
+      {label}
+    </span>
+  )
+}
