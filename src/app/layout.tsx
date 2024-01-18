a3317ccded6acc020f21be69afad081fde8f4aa3
Alex Calle <chikyelfurioso2011@gmail.com>
2024-01-17 21:20:12 -0500

    Refactor import statement in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Header } from '@/components/Header/Header'
+import { Header } from '@/components/Header'
 import clsx from 'clsx'
 import { Inter } from 'next/font/google'
 import LocalFont from 'next/font/local'
