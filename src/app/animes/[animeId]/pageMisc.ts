fa34c36019dc05931594fc14722bb0eaf0a2f49a
Alex Calle <chikyelfurioso2011@gmail.com>
2024-01-18 21:42:59 -0500

    Refactor pageMisc.ts to improve metadata generation

--- src/app/animes/[animeId]/pageMisc.ts
+++ src/app/animes/[animeId]/pageMisc.ts
@@ -2,49 +2,42 @@ import { getAnime } from '@/services/getAnime'
 import { getBroadcastAnimes } from '@/services/getBroadcastAnimes'
 import { getRatingAnimes } from '@/services/getRatingAnimes'
 import { Anime } from '@/types'
+import { normalizeAnimeId } from '@/utils/normalizeAnimeId'
 import { Metadata } from 'next'
 
 export interface PageProps {
-  params: {
-    animeId: string
-  }
-  searchParams: {
-    limit: string
-  }
+  params: { animeId: string }
+  searchParams: { limit: string }
 }
 
-export async function generateMetadataFromAnimeId(
-  animeId: PageProps['params']['animeId']
-): Promise<Metadata> {
+export async function generateMetadataFromAnimeId(animeId: PageProps['params']['animeId']): Promise<Metadata> {
   const anime = await getAnime(animeId)
-
   if (!anime) return {}
 
-  const title = anime.title ?? 'Anime'
-  const keywords = `${anime.genres?.join(', ')} ${anime.title} ${anime.otherTitles?.join(', ')}`
-
   return {
-    title,
+    title: anime.title ?? normalizeAnimeId(animeId),
     description: anime.description,
-    keywords
+    keywords: `${anime.genres.join(', ')} ${anime.title} ${anime.otherTitles.join(', ')}`
   }
 }
 
-export async function generatePageStaticParams(): Promise<
-  { animeId: string }[]
-> {
+export async function generatePageStaticParams(): Promise<{ animeId: string }[]> {
   const animesPromises = [getRatingAnimes(25), getBroadcastAnimes()]
+
   const animesIdSettled = await Promise.allSettled(
     animesPromises.map(animePromise =>
       animePromise.then(anime => anime.map(({ animeId }) => animeId))
     )
   )
+
   const filteredAnimesId = animesIdSettled.filter(
     (animesId): animesId is PromiseFulfilledResult<Anime['animeId'][]> =>
       animesId.status === 'fulfilled'
   )
+
   const mappedAnimesId = filteredAnimesId.flatMap(filteredAnimesId =>
     filteredAnimesId.value.map(animeId => ({ animeId }))
   )
+  
   return mappedAnimesId
 }
