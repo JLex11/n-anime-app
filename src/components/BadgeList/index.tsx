55d0446bf80fb9eb96f89b26ce9d3afd7cab1889
Alex Calle <chikyelfurioso2011@gmail.com>
2024-01-21 21:37:38 -0500

    Add BadgeList component

---
+++ src/components/BadgeList/index.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link'
+import { Badge } from '../Common/Badge'
+import styles from './BadgeList.module.css'
+
+interface Props {
+  items: {
+    name: string
+    url?: string
+  }[],
+  width?: string
+  columnGap?: string
+  rowGap?: string
+}
+
+interface CSSProps extends React.CSSProperties {
+  '--badge-width': string
+  '--badge-column-gap': string
+  '--badge-row-gap': string
+}
+
+export function BadgeList({ items, width, columnGap, rowGap }: Props) {
+  const cssProps: CSSProps = {
+    '--badge-width': width || 'auto',
+    '--badge-column-gap': columnGap || '0.5rem',
+    '--badge-row-gap': rowGap || '1rem'
+  }
+
+  return (
+    <ul className={styles.badgeList} style={cssProps}>
+      {items.map(item => (
+        <li key={item.name}>
+          <Badge>
+            {item.url ? <Link href={item.url}>{item.name}</Link> : item.name}
+          </Badge>
+        </li>
+      ))}
+    </ul>
+  )
+}
