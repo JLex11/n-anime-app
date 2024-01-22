87c5fb200c69f8eeeffd33800849900c70ed73c1
Alex Calle <chikyelfurioso2011@gmail.com>
2024-01-21 21:37:45 -0500

    Refactor GenresSection component to use BadgeList

--- src/components/AnimePage/GenresSection.tsx
+++ src/components/AnimePage/GenresSection.tsx
@@ -1,22 +1,19 @@
-import { Badge } from '@/components/Common/Badge'
-import Link from 'next/link'
+import { BadgeList } from '../BadgeList'
 import styles from './Anime.module.css'
 
 export function Genres({ genres }: { genres: string[] }) {
   if (genres.length === 0) return null
 
+  const uniqueGenres = [...new Set(genres)]
+  const mappedGenres = uniqueGenres.map(genre => ({
+    name: genre,
+    url: `/animes/?genre=${genre}`
+  }))
+
   return (
     <section className={styles.section}>
       <h2 className={styles.sectionTitle}>Generos</h2>
-      <ul className={styles.sectionList}>
-        {genres.map(genre => (
-          <li key={genre}>
-            <Badge>
-              <Link href={`/search/?genre=${genre}`}>{genre}</Link>
-            </Badge>
-          </li>
-        ))}
-      </ul>
+      <BadgeList items={mappedGenres} />
     </section>
   )
 }
