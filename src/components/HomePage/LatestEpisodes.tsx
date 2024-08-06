15e4f3ec8b491ea7589705056d6e59c02b6135a7
Lexx <chikyelfurioso2011@gmail.com>
2024-08-06 12:49:52 -0500

    format

--- src/components/HomePage/LatestEpisodes.tsx
+++ src/components/HomePage/LatestEpisodes.tsx
@@ -3,28 +3,27 @@ import { Card } from '../Card'
 import { CardSkeleton } from '../Card/CardSkeleton'
 
 export async function LatestEpisodes() {
-  const latestEpisodes = await getLatestEpisodes()
+	const latestEpisodes = await getLatestEpisodes()
 
-  const episodeData = latestEpisodes.map((episode, index) => {
-    return {
-      key: episode.episodeId,
-      image: {
-        src: episode.image ?? '',
-        width: 350,
-        height: 250,
-        loading: index < 3 ? ('eager' as const) : ('lazy' as const)
-      },
-      title: episode.title,
-      link: `/animes/${episode.animeId}/${episode.episode}`,
-      pill: { label: `Episodio ${episode.episode}` }
-    }
-  })
+	const episodeData = latestEpisodes.map((episode, index) => {
+		return {
+			key: episode.episodeId,
+			image: {
+				src: episode.image ?? '',
+				width: 350,
+				height: 250,
+				loading: index < 3 ? ('eager' as const) : ('lazy' as const),
+			},
+			title: episode.title,
+			link: `/animes/${episode.animeId}/${episode.episode}`,
+			prefetch: true,
+			pill: { label: `Episodio ${episode.episode}` },
+		}
+	})
 
-  return episodeData.map(({ key, title, link, pill, image }) => (
-    <Card key={key} image={image} title={title} link={link} pill={pill} />
-  ))
+	return episodeData.map(({ key, ...props }) => <Card key={key} {...props} />)
 }
 
 export function CardsSkeleton({ countCards, hasPill }: { countCards: number; hasPill?: boolean }) {
-  return new Array(countCards).fill(0).map((_, i) => <CardSkeleton key={i} hasPill={hasPill} />)
+	return new Array(countCards).fill(0).map((_, i) => <CardSkeleton key={i} hasPill={hasPill} />)
 }
