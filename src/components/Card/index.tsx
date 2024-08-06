5a52fea78e4cbeaae83b50152e377cdfec54cca9
Lexx <chikyelfurioso2011@gmail.com>
2024-08-06 12:49:09 -0500

    adding prefetching param

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,27 +2,27 @@ import { Link } from 'next-view-transitions'
 import { Pill } from '../Pill'
 import styles from './Card.module.css'
 import { CardImage } from './CardImage'
-import { CardProps } from './types'
+import type { CardProps } from './types'
 
-export function Card({ title, link, image, pill, showOnHover, animeId }: CardProps) {
-  return (
-    <article className={styles.card}>
-      <Link href={link} className={styles.card_anchor}>
-        <CardImage
-          {...image}
-          alt={title}
-          className={styles.card_img}
-          decoding='async'
-          style={animeId ? { viewTransitionName: `anime-image-${animeId}` } : {}}
-        />
-        <div className={styles.content}>
-          {pill?.label && <Pill label={pill.label} className={styles.pill} />}
-          <h3 className={styles.title} style={animeId ? { viewTransitionName: `anime-title-${animeId}` } : {}}>
-            {title}
-          </h3>
-        </div>
-      </Link>
-      {showOnHover && <div className={styles.showOnHoverContainer}>{showOnHover}</div>}
-    </article>
-  )
+export function Card({ title, link, prefetch, image, pill, showOnHover, animeId }: CardProps) {
+	return (
+		<article className={styles.card}>
+			<Link href={link} className={styles.card_anchor} prefetch={prefetch}>
+				<CardImage
+					{...image}
+					alt={title}
+					className={styles.card_img}
+					decoding='async'
+					style={animeId ? { viewTransitionName: `anime-image-${animeId}` } : {}}
+				/>
+				<div className={styles.content}>
+					{pill?.label && <Pill label={pill.label} className={styles.pill} />}
+					<h3 className={styles.title} style={animeId ? { viewTransitionName: `anime-title-${animeId}` } : {}}>
+						{title}
+					</h3>
+				</div>
+			</Link>
+			{showOnHover && <div className={styles.showOnHoverContainer}>{showOnHover}</div>}
+		</article>
+	)
 }
--- src/components/Card/types.ts
+++ src/components/Card/types.ts
@@ -1,18 +1,19 @@
 import Image from 'next/image'
-import { PillProps } from '../Pill'
+import type { PillProps } from '../Pill'
 
 export interface ImageProps extends Omit<React.ComponentProps<typeof Image>, 'src' | 'width' | 'height'> {
-  src: string | null | undefined
-  fbSrc?: string
-  width: number
-  height: number
+	src: string | null | undefined
+	fbSrc?: string
+	width: number
+	height: number
 }
 
 export interface CardProps {
-  title: string
-  link: string
-  image: Omit<ImageProps, 'alt'>
-  pill?: PillProps
-  showOnHover?: React.ReactNode
-  animeId?: string
+	title: string
+	link: string
+	prefetch?: boolean
+	image: Omit<ImageProps, 'alt'>
+	pill?: PillProps
+	showOnHover?: React.ReactNode
+	animeId?: string
 }
