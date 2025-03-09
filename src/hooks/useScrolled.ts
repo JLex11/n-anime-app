71a118049a24b8d199e6f1b8f5eba8c0ed5a5b98
Lexx <chikyelfurioso2011@gmail.com>
2025-03-09 15:39:23 -0500

    some lint issues

--- src/hooks/useScrolled.ts
+++ src/hooks/useScrolled.ts
@@ -1,13 +1,13 @@
 import { viewHeight } from '@/utils/calculateClientViewport'
 import { useEffect, useState } from 'react'
 
-export const useScrolled = (vh: number = 30) => {
+export const useScrolled = (vh = 30) => {
   const [scrolled, setScrolled] = useState(false)
 
   useEffect(() => {
     const handleScroll = () => {
       const s = window.scrollY > viewHeight(vh)
-      if (s != scrolled) setScrolled(s)
+      if (s !== scrolled) setScrolled(s)
     }
 
     window.addEventListener('scroll', handleScroll, { passive: true })
