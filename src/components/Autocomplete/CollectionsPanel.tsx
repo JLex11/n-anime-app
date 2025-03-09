4c96adb0fa52bcc886159ee25abb7c07771bf737
Lexx <chikyelfurioso2011@gmail.com>
2025-03-09 10:21:42 -0500

    format

--- src/components/Autocomplete/CollectionsPanel.tsx
+++ src/components/Autocomplete/CollectionsPanel.tsx
@@ -1,41 +1,33 @@
-import { AutocompleteOutputItem } from '@/hooks/useAutocomplete.types'
+import type { AutocompleteOutputItem } from '@/hooks/useAutocomplete.types'
 import styles from './Autocomplete.module.css'
 import { Collection } from './Collection'
 import { PanelFooter } from './PanelFooter'
 
 interface Props {
-  className: string
-  panelRef: React.RefObject<HTMLDivElement>
-  panelProps: {
-    onMouseDown(event: MouseEvent): void
-    onMouseLeave(): void
-  }
-  collections: {
-    items: AutocompleteOutputItem[]
-    source: {
-      sourceId: string
-    }
-  }[]
+	className: string
+	panelRef: React.RefObject<HTMLDivElement>
+	panelProps: {
+		onMouseDown(event: MouseEvent): void
+		onMouseLeave(): void
+	}
+	collections: {
+		items: AutocompleteOutputItem[]
+		source: {
+			sourceId: string
+		}
+	}[]
 }
 
-export function CollectionsPanel({
-  className: cssClass,
-  panelRef,
-  panelProps,
-  collections
-}: Props) {
-  return (
-    <div className={cssClass} ref={panelRef} {...(panelProps as any)}>
-      <div className={styles.collectionsContainer}>
-        {collections.map(collection => (
-          <Collection
-            key={collection.source.sourceId}
-            sourceId={collection.source.sourceId}
-            items={collection.items}
-          />
-        ))}
-      </div>
-      <PanelFooter />
-    </div>
-  )
+export function CollectionsPanel({ className: cssClass, panelRef, panelProps, collections }: Props) {
+	return (
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		<div className={cssClass} ref={panelRef} {...(panelProps as any)}>
+			<div className={styles.collectionsContainer}>
+				{collections.map(collection => (
+					<Collection key={collection.source.sourceId} sourceId={collection.source.sourceId} items={collection.items} />
+				))}
+			</div>
+			<PanelFooter />
+		</div>
+	)
 }
