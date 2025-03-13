3e3bd028af637c8b92922a755e2bc4e2dd1e4cde
Lexx <chikyelfurioso2011@gmail.com>
2025-03-13 18:42:50 -0500

    using import type

--- src/components/ServerAutocomplete/AutocompleteItem.tsx
+++ src/components/ServerAutocomplete/AutocompleteItem.tsx
@@ -1,36 +1,36 @@
 import Link from 'next/link'
 import styles from './ServerAutocomplete.module.css'
-import { Item } from './autocompleteTypes'
+import type { Item } from './autocompleteTypes'
 
 interface Props {
-  item: Item
+	item: Item
 }
 
 export function AutocompleteItem({ item }: Props) {
-  return (
-    <li className={styles.item}>
-      <Link href={item.link} className={styles.itemContainer}>
-        {typeof item.image === 'string' ? (
-          <img
-            src={item.image}
-            alt={item.title}
-            width={50}
-            height={50}
-            className={styles.itemImage}
-            loading='lazy'
-            decoding='async'
-          />
-        ) : (
-          item.image
-        )}
-        <div className={styles.itemInfo}>
-          <div className={styles.itemContent}>
-            <h4 className={styles.itemTitle}>{item.title}</h4>
-            <p className={styles.itemDescription}>{item.description.slice(0, 100)}</p>
-          </div>
-        </div>
-        <span className={styles.itemType}>{item.type}</span>
-      </Link>
-    </li>
-  )
+	return (
+		<li className={styles.item}>
+			<Link href={item.link} className={styles.itemContainer}>
+				{typeof item.image === 'string' ? (
+					<img
+						src={item.image}
+						alt={item.title}
+						width={50}
+						height={50}
+						className={styles.itemImage}
+						loading='lazy'
+						decoding='async'
+					/>
+				) : (
+					item.image
+				)}
+				<div className={styles.itemInfo}>
+					<div className={styles.itemContent}>
+						<h4 className={styles.itemTitle}>{item.title}</h4>
+						<p className={styles.itemDescription}>{item.description.slice(0, 100)}</p>
+					</div>
+				</div>
+				<span className={styles.itemType}>{item.type}</span>
+			</Link>
+		</li>
+	)
 }
