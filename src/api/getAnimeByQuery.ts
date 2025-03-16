cbb6606aca53de68a23c5caa3b0dc8a69796a53e
Lexx <chikyelfurioso2011@gmail.com>
2025-03-16 16:24:13 -0500

    actualizar getAnimesByQuery para soportar paginación y reducir el tiempo de revalidación

--- src/api/getAnimeByQuery.ts
+++ src/api/getAnimeByQuery.ts
@@ -3,10 +3,12 @@ import { fetchAnimeHelper } from '@/services/fetchAnimeHelper'
 import type { Anime } from '@/types'
 import { minToSeconds } from '@/utils/convertTime'
 
-export const getAnimesByQuery = async (query: string, limit?: number): Promise<Anime[]> => {
-  return await fetchAnimeHelper<Anime[]>(
-    `${APIRoutes.SearchAnimes}/${query}`,
-    { revalidate: minToSeconds(30) },
-    { limit }
-  ) || []
+export const getAnimesByQuery = async (query: string, limit?: number, page?: number): Promise<Anime[]> => {
+	return (
+		(await fetchAnimeHelper<Anime[]>(
+			`${APIRoutes.SearchAnimes}/${query}`,
+			{ revalidate: minToSeconds(1) },
+			{ pageSize: limit, page }
+		)) || []
+	)
 }
