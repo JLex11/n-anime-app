3f89595cd849e4238e77833eb27bb4707780e7f7
Lexx <chikyelfurioso2011@gmail.com>
2025-03-23 15:24:14 -0500

    refactor: eliminar ViewTransition en el componente Carousel para simplificar la estructura

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,7 +1,6 @@
 import type { Anime } from '@/types'
 import { CarouselWrapper } from './CarouselWrapper'
 import { Item } from './Item'
-import { unstable_ViewTransition as ViewTransition } from 'react'
 
 interface Props {
 	animes: Anime[]
@@ -12,12 +11,10 @@ export function Carousel({ animes, showInfo }: Props) {
 	if (!animes) return null
 
 	return (
-		<ViewTransition name='carousel'>
-			<CarouselWrapper animes={animes}>
-				{animes.map((anime, i) => (
-					<Item key={anime.animeId} anime={anime} showInfo={showInfo} index={i} />
-				))}
-			</CarouselWrapper>
-		</ViewTransition>
+		<CarouselWrapper animes={animes}>
+			{animes.map((anime, i) => (
+				<Item key={anime.animeId} anime={anime} showInfo={showInfo} index={i} />
+			))}
+		</CarouselWrapper>
 	)
 }
