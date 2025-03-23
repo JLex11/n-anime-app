e15c40807492239b868c48bc5b321a4144918acd
Lexx <chikyelfurioso2011@gmail.com>
2025-03-23 15:12:14 -0500

    refactor: eliminar useLayoutEffect en useCarousel para simplificar la lógica y mejorar la compatibilidad con SSR

--- src/hooks/useCarousel.ts
+++ src/hooks/useCarousel.ts
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 interface Props {
 	itemIds: string[]
@@ -11,9 +11,6 @@ interface CurrentItem {
 
 const SESSION_STORAGE_KEY = 'currentSlideId'
 
-// Creamos una versión segura de useLayoutEffect que funciona en SSR
-const useIsomorphicLayoutEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect
-
 export function useCarousel({ itemIds }: Props) {
 	const [currentItem, setCurrentItem] = useState<CurrentItem>({
 		value: 0,
@@ -22,8 +19,7 @@ export function useCarousel({ itemIds }: Props) {
 
 	const scrollerRef = useRef<HTMLUListElement>(null)
 
-	// Usamos useIsomorphicLayoutEffect para que se ejecute solo una vez al montar el componente
-	useIsomorphicLayoutEffect(() => {
+	useEffect(() => {
 		if (typeof window !== 'undefined') {
 			const currentSlideId = sessionStorage.getItem(SESSION_STORAGE_KEY)
 			const currentSlideIndex = currentSlideId ? itemIds.indexOf(currentSlideId) : -1
