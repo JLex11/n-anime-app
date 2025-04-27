ef5d69d54ad0b1792672543038f95fae066ceffb
Lexx <chikyelfurioso2011@gmail.com>
2025-04-27 09:19:08 -0500

    refactor: mejorar la legibilidad del código en SearchLauncher eliminando código comentado

--- src/components/Autocomplete/SearchLauncher.tsx
+++ src/components/Autocomplete/SearchLauncher.tsx
@@ -8,47 +8,30 @@ import { useHotkeys } from 'react-hotkeys-hook'
 const Autocomplete = dynamic(() => import('.').then(mod => mod.Autocomplete))
 
 interface Props {
-  className?: string
-  children?: React.ReactNode
+	className?: string
+	children?: React.ReactNode
 }
 
 export function SearchLauncher({ className: cssClass, children }: Props) {
-  const [autocompleteLaunched, setAutocompleteLaunched] = useState(false)
+	const [autocompleteLaunched, setAutocompleteLaunched] = useState(false)
 
-  const handleToggleSearch = () => setAutocompleteLaunched(!autocompleteLaunched)
-  const handleCloseAutocomplete = () => setAutocompleteLaunched(false)
+	const handleToggleSearch = () => setAutocompleteLaunched(!autocompleteLaunched)
+	const handleCloseAutocomplete = () => setAutocompleteLaunched(false)
 
-  const hotkeysOptions = {
-    preventDefault: true,
-    enableOnFormTags: true
-  }
+	const hotkeysOptions = {
+		preventDefault: true,
+		enableOnFormTags: true,
+	}
 
-  useHotkeys(autoCompleteHotKeys.LAUNCH, handleToggleSearch, hotkeysOptions)
-  useHotkeys(autoCompleteHotKeys.OUT, handleCloseAutocomplete, hotkeysOptions)
+	useHotkeys(autoCompleteHotKeys.LAUNCH, handleToggleSearch, hotkeysOptions)
+	useHotkeys(autoCompleteHotKeys.OUT, handleCloseAutocomplete, hotkeysOptions)
 
-  return (
-    <>
-      <button onClick={() => setAutocompleteLaunched(true)} className={cssClass} type='button'>
-        {children}
-      </button>
-      {autocompleteLaunched && <Autocomplete handleLaunchAutocomplete={setAutocompleteLaunched} />}
-    </>
-  )
+	return (
+		<>
+			<button onClick={() => setAutocompleteLaunched(true)} className={cssClass} type='button'>
+				{children}
+			</button>
+			{autocompleteLaunched && <Autocomplete handleLaunchAutocomplete={setAutocompleteLaunched} />}
+		</>
+	)
 }
-
-/* const searchParams = useSearchParams()
-const router = useRouter()
-
-const handleToggleSearch = () => {
-  const searchParamsEntries = [...searchParams.entries()]
-  !searchParamsEntries.find(([key]) => key === AutocompleteVars.QUERY_NAME) && searchParamsEntries.push([AutocompleteVars.QUERY_NAME, ''])
-
-  router.push(`?${searchParamsEntries.map(([key, value]) => `${key}=${value}`).join('&')}`)
-}
-
-const handleCloseAutocomplete = () => {
-  const searchParamsEntries = [...searchParams.entries()]
-  const filteredSearchParams = searchParamsEntries.filter(([key]) => key !== AutocompleteVars.QUERY_NAME)
-
-  router.push(`?${filteredSearchParams.map(([key, value]) => `${key}=${value}`).join('&')}`)
-} */
