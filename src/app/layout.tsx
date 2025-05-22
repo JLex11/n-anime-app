e386d862071d5503bede4024e6577f08c00a9004
Lexx <chikyelfurioso2011@gmail.com>
2025-05-22 18:40:10 -0500

    refactor: añadir tipo Metadata a la exportación de metadata en layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import { Header } from '@/components/Header'
 import clsx from 'clsx'
+import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import LocalFont from 'next/font/local'
 import Script from 'next/script'
 import { unstable_ViewTransition as ViewTransition } from 'react'
 import '../globals.css'
 
-export const metadata = {
+export const metadata: Metadata = {
 	title: 'One Anime',
 	description:
 		'Descubre un mundo lleno de tus animes favoritos y sumérgete en la magia del entretenimiento en línea. En este lugar, podrás disfrutar de una amplia selección de series animadas japonesas que te transportarán a emocionantes aventuras, intrigantes historias y personajes inolvidables. Desde los clásicos más queridos hasta los lanzamientos más recientes, aquí encontrarás todo lo que necesitas para saciar tu pasión por el anime. No importa si eres un fanático de la acción, la comedia, el romance o la fantasía, nuestra plataforma te brinda la oportunidad de disfrutar de tus animes favoritos en cualquier momento y en cualquier lugar. ¡Únete a nuestra comunidad anime y déjate llevar por la emoción de este fascinante universo!',
