6139834df6e2fd4a6cbe89ff57af968f1fa7d37b
Lexx <chikyelfurioso2011@gmail.com>
2025-05-22 18:40:02 -0500

    refactor: ajustar la configuración de fetchConfig y mejorar la legibilidad del código en getAnimeEpisodes

--- src/api/getAnimeEpisodes.ts
+++ src/api/getAnimeEpisodes.ts
@@ -4,13 +4,14 @@ import { minToSeconds } from '@/utils/convertTime'
 import { fetchData } from '../services/fetchData'
 
 export const getAnimeEpisodes = async (animeId: string, offset?: number, limit?: number) => {
-  const fetchConfig = {
-    next: { revalidate: minToSeconds(30) }
-  }
+	const fetchConfig = {
+		next: { revalidate: minToSeconds(30) },
+	}
 
-  const animeEpisodes = await fetchData<Episode[]>(
-    `${APIRoutes.AnimeEpisodes.replace(':animeId', animeId)}?offset=${offset}&limit=${limit}`,
-    fetchConfig
-  )
-  return animeEpisodes || []
+	const animeEpisodes = await fetchData<Episode[]>(
+		`${APIRoutes.AnimeEpisodes.replace(':animeId', animeId)}?offset=${offset}&limit=${limit}`,
+		fetchConfig
+	)
+
+	return animeEpisodes || []
 }
